18b1f6e99cdac51b428129a83a427da5524ce265
Jeff Hykin <jeffhykin@jeffs-imac.lan>
2019-08-09 23:33:49 -0500

    _switch to FS class

--- setup.rb
+++ setup.rb
@@ -4,14 +4,14 @@ require 'atk_toolbox'
 # 
 # create the file structure if it doesnt exist
 # 
-FileUtils.makedirs(HOME/"atk"/"installers")
-FileUtils.makedirs(HOME/"atk"/"temp")
+FS.makedirs(HOME/"atk"/"installers")
+FS.makedirs(HOME/"atk"/"temp")
 # download the files
-if not File.exist?(HOME/"atk"/"core.yaml")
-    download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/core.yaml'       , as: HOME/"atk"/"core.yaml")
+if not FS.exist?(HOME/"atk"/"core.yaml")
+    FS.download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/core.yaml'       , to: HOME/"atk"/"core.yaml")
 end
-if not File.exist?(HOME/"atk"/"installers.yaml")
-    download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/installers.yaml' , as: HOME/"atk"/"installers.yaml")
+if not FS.exist?(HOME/"atk"/"installers.yaml")
+    FS.download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/installers.yaml' , to: HOME/"atk"/"installers.yaml")
 end
 
 #
@@ -20,18 +20,18 @@ end
 
 # atk
 atk_command_download_path = HOME/"atk"/"temp"/"atk.rb"
-download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/atk'     , as: atk_command_download_path)
-set_command("atk", IO.read(atk_command_download_path))
+FS.download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/atk'     , to: atk_command_download_path)
+set_command("atk", FS.read(atk_command_download_path))
 
 # project
 project_command_download_path = HOME/"atk"/"temp"/"project.rb"
-download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/project' , as: project_command_download_path)
-set_command("project", IO.read(project_command_download_path))
+FS.download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/project' , to: project_command_download_path)
+set_command("project", FS.read(project_command_download_path))
 
 # >
 local_command_download_path = HOME/"atk"/"temp"/"doubledash_command.rb"
-download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/>'      , as: local_command_download_path)
-set_command(">", IO.read(local_command_download_path))
+FS.download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/>'      , to: local_command_download_path)
+set_command(">", FS.read(local_command_download_path))
 
 # 
 # print success
