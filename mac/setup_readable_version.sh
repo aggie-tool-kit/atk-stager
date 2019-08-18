c2e9b1312caea2e09219b9053c87c7069d40cd92
Jeff Hykin <jeffhykin@Jeffs-iMac.local>
2019-08-18 02:34:21 -0500

    _

--- mac/setup_readable_version.sh
+++ mac/setup_readable_version.sh
@@ -2,14 +2,15 @@
 sudo echo ""
 # install homebrew if not installed
 which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# install python3 if not installed
-which python3 || brew install python3 &> /dev/null
-# install asciimatics
-pip3 install asciimatics &> /dev/null
 # create the temp dir
 mkdir -p ~/atk/temp
-# run installation in the background
+# install git if needed
 which git || brew install git
+# install ruby 2.5.5
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
+rbenv versions | grep '2.5.5' || echo 'installing ruby 2.5.5... this might take a little bit' && rbenv install 2.5.5
+rbenv init
+rbenv global 2.5.5
 # install the atk_toolbox gem
 gem install atk_toolbox
 # download the setup.rb
